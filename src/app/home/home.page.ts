356f4612d9d77137469f279c2e5c8ae919ce9989
Biplab Sharma <jobs.biplab.sharma@gmail.com>
2023-10-19 00:38:20 +0530

    addddddd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Filesystem, Encoding } from '@capacitor/filesystem';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  parentPath = 'file:///storage/emulated/0/';
+  constructor(private alertController: AlertController) {}
 
-  constructor() { }
-  
   public async AddFolder() {
-    
-  }
+    try {
+      await Filesystem.writeFile({
+        path: `${this.parentPath}'IonicFolder/IonicData.txt'`,
+        data: 'This is a test',
+        encoding: Encoding.UTF8,
+      });
+    } catch (error:any) {
 
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        subHeader: 'An Error Occured',
+        message: error,
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+
+      await Filesystem.mkdir({
+        path: `${this.parentPath}'IonicFolder`,
+        recursive: false,
+      });
+
+      await Filesystem.writeFile({
+        path: `${this.parentPath}'IonicFolder/IonicData.txt'`,
+        data: 'This is a test',
+        encoding: Encoding.UTF8,
+      });
+
+       const alert2 = await this.alertController.create({
+        header: 'Alert',
+        subHeader: 'Success',
+        message: 'Successfull',
+        buttons: ['OK'],
+      });
+
+      await alert2.present();
+    }
+  }
 }
